988c3fc6045cedad3a1b34e6c2ce8fd361d05a97
Fernando Lima <tosta.fernando@gmail.com>
2025-09-09 13:07:43 -0300

    Fix cookie await

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,38 +1,46 @@
 // src/app/auth/callback/route.ts
-
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
-import { type CookieOptions, createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { type CookieOptions, createServerClient } from '@supabase/ssr';
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get('code')
-  const next = searchParams.get('next') ?? '/speech2text'
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get('code');
+  const next = searchParams.get('next') ?? '/speech2text';
 
   if (code) {
-    const cookieStore = cookies()
+    const cookieStore = await cookies();
+    
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
           get(name: string) {
-            return cookieStore.get(name)?.value
+            return cookieStore.get(name)?.value;
           },
           set(name: string, value: string, options: CookieOptions) {
-            cookieStore.set({ name, value, ...options })
+            try {
+              cookieStore.set({ name, value, ...options });
+            } catch (error) {
+            }
           },
           remove(name: string, options: CookieOptions) {
-            cookieStore.set({ name, value: '', ...options })
+            try {
+              cookieStore.set({ name, value: '', ...options });
+            } catch (error) {
+            }
           },
         },
       }
-    )
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    );
+
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
-  return NextResponse.redirect(`${origin}/login?error=auth-code-error`)
+  // Redireciona para uma página de erro se o login falhar
+  return NextResponse.redirect(`${origin}/login?error=auth-code-error`);
 }
